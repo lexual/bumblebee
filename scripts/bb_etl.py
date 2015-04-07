703fd5531b808481b36a21827465324ad9432c30
lexual <lex@jbadigital.com>
2015-04-07 15:19:50 +1000

    script write output to standard out.

--- scripts/bb_etl.py
+++ scripts/bb_etl.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import bumblebee
 import bumblebee as bb
+from StringIO import StringIO
 import sys
 
 usage = """
 HELP
-bb_etl.py input.csv transformation_rules.yaml output.csv
+bb_etl.py input.csv transformation_rules.yaml > output.csv
 """
 
 def main():
     try:
         args = sys.argv[1:]
-        input_csv, transformation_file, output_csv = args
+        input_csv, transformation_file = args
     except:
-        print usage
+        print(usage)
         return
     convertor = bb.Convertor.from_yaml(transformation_file)
     output = convertor.transform(input_csv)
-    output.to_csv(output_csv, index=False)
-    print 'Transformation successful!'
+    output_buffer = StringIO()
+    output.to_csv(output_buffer, index=False)
+    output_buffer.seek(0)
+    print(output_buffer.read(), end='')
 
 
 if __name__ == '__main__':
